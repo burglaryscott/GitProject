bd90ea09bca06d2a620d1c1fd130ec781b48b4df
mac <mac@192.168.144.141>
2024-03-01 22:37:43 +0530

    ok it's third

--- src/com/user/folder/FirstClass.java
+++ src/com/user/folder/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("this is testing");
 		System.out.println("new commit");
+		System.out.println("third time to pre-master not master");
 	}
 }
